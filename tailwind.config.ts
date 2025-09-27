56f259e78d46bfa1da9de7d120a2c4328a70b611
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-09-27 22:51:47 +0530

    Tailwind and Colors Setup

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#EEEEEE",
+        primary: "#00ADB5",
+        secondary: "#393E46",
+        text: "#222831",
+      },
+      fontFamily: {
+        sans: ["var(--font-poppins)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
